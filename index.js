7175c63a642e281b92ca8f0411930c6429e55f69
thistonyb <this.tonybaker@gmail.com>
2019-10-29 18:06:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(driversArray, driver) {
+  return driversArray.filter(
+    driverMatch => driverMatch.toLowerCase() === driver.toLowerCase()
+  );
+}
+
+function fuzzyMatch(driversArray, letter) {
+  return driversArray.filter(
+    driverMatch => driverMatch.toLowerCase().indexOf(letter.toLowerCase()) === 0
+  );
+}
+
+function matchName(driversArray, exactName) {
+  return driversArray.filter(driver => driver.name === exactName);
+}
